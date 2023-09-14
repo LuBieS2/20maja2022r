4a38ca5b48f55ce0c43c03010805fddadd1bee22
LuBieS2 <119162617+LuBieS2@users.noreply.github.com>
2023-09-14 22:46:44 +0200

    bmjbvnmk,

--- 20maja2022r/_20maja2022r.py
+++ 20maja2022r/_20maja2022r.py
@@ -1,3 +1,4 @@
+#https://cke.gov.pl/images/_EGZAMIN_MATURALNY_OD_2015/Arkusze_egzaminacyjne/2022/Informatyka/poziom_rozszerzony/EINP-R2-100-2205.pdf
 def prime(a):
     if a==1 or a<1:
         return False
@@ -16,9 +17,8 @@ def prime(a):
         else:
             return True
 
-file=open("orzyklad.txt", "r")
+file=open("przyklad.txt", "r")
 numbers= list(map(str.strip, file.readlines()))
-
 #4.1
 digits=[]
 temp_counter=[]
@@ -42,8 +42,7 @@ n_hpu=""
 for i in numbers:
     print(i)
     c=i
-    while int(i)>1:
-        
+    while int(i)>1:    
         for x in range(1, int(i)+1):
             b=x
             if int(i)%x==0:
@@ -62,3 +61,23 @@ for i in numbers:
     primen=[]        
 print(highest_primes, n_hp)
 print(h_pu, n_hpu)
+#4.3
+threes=[]
+numbers = [eval(i) for i in numbers]
+fives=[]
+for i in numbers:
+    for g in numbers:
+        if i%g==0 and i>g:
+            for n in numbers:
+                if g%n== 0 and g>n:
+                    for j in numbers:
+                        if n%j==0 and n>j:
+                            for h in numbers:
+                                if j%h==0 and j>h:
+                                    fives.append([h, j, n, g, i])
+                    threes.append([n, g ,i])
+
+print("5",fives)
+print("3",threes)
+print(len(fives))
+print(len(threes))
